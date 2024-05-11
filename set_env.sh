7b7eb956e9b0c02ce8c2deef120b6b13b47586fa
Kien Hoang <kienhoang1891997@gmail.com>
2024-05-11 22:44:17 +0700

    Initialize

---
+++ set_env.sh
@@ -0,0 +1,14 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=kienhoang
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagramdb.cwaaphhsm30v.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+
+export AWS_BUCKET=arn:aws:s3:::udagram-213761293842-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+
+
