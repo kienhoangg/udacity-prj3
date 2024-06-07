196cd509b386faf1bb703586a9b36804172184d1
Kien Hoang <kienhoang1891997@gmail.com>
2024-06-07 22:24:57 +0700

    update deployment

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=kienhoang
 export POSTGRES_PASSWORD=kienhoang
-export POSTGRES_HOST=udagramdb.cwaaphhsm30v.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=udagramdb.cdgs8qqjr4pz.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 
 export AWS_BUCKET=arn:aws:s3:::udagram-213761293842-dev
