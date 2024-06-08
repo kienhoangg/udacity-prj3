541bc942a537944b6fba82bb213720397252c303
Kien Hoang <kienhoang1891997@gmail.com>
2024-06-08 12:25:13 +0700

    update aws elb route

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,8 +5,9 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
-};
+  apiHost:
+    'http://a300b237461e94170add65b623e96fe9-1081972803.us-east-1.elb.amazonaws.com:8080/api/v0',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
